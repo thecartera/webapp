88c540393affe220bc9ae663c77c16d1e3e1c4ea
Joao Meyer <1994meyer@gmail.com>
2021-03-22 19:15:46 -0300

    Added navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#"> Monneda </b-navbar-brand>
+
+    <b-navbar-nav>
+      <b-nav-item href="#"> Home </b-nav-item>
+      <b-nav-item href="#"> Disabled </b-nav-item>
+    </b-navbar-nav>
+
+    <b-navbar-nav class="ml-auto">
+      <b-nav-item> Profile </b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
