96bc1c1a07c3603e6639d985728d79c274b2024c
Joao Meyer <1994meyer@gmail.com>
2021-03-23 13:36:48 -0300

    Customizing wallet viewer

--- src/components/Wallet.vue
+++ src/components/Wallet.vue
@@ -3,18 +3,19 @@
     <b-card-title>
       <b-row>
         <b-col> {{ wallet.id }} </b-col>
-        <b-col
-          class="text-right"
-          :class="style"
-        >
-          {{ monthly }}%
-        </b-col>
+        <b-col class="text-right" :class="style"> {{ monthly }}% </b-col>
       </b-row>
     </b-card-title>
     <b-card-sub-title> {{ created }} </b-card-sub-title>
 
     <b-card-body>
-      <b-table hover :items="wallet.assets" />
+      <b-table hover :fields="fields" :items="wallet.assets">
+        <template #cell(ticker)="data"> {{ data.value.toUpperCase() }} </template>
+        <template #cell(price)="data"> R$ {{ round(data.value) }} </template>
+        <template #cell(weight)="data"> {{ round(data.value) }} % </template>
+        <template #cell(monthlyGain)="data"> {{ round(data.value) }} % </template>
+        <template #cell(date)="data"> {{ new Date(data.value).toDateString() }} </template>
+      </b-table>
     </b-card-body>
   </b-card>
 </template>
@@ -25,10 +26,20 @@ export default {
 
   props: {
     wallet: {
-      default: () => ({}),
+      default: () => ({ assets: [] }),
       type: Object
     }
   },
+
+  data: () => ({
+    fields: [
+      { key: 'ticker', label: 'Código' },
+      { key: 'price', label: 'Preço' },
+      { key: 'weight', label: 'Peso' },
+      { key: 'date', label: 'Data' },
+      { key: 'monthlyGain', label: 'Lucro mensal' }
+    ]
+  }),
 
   computed: {
     created () {
@@ -41,6 +52,12 @@ export default {
 
     style () {
       return this.monthly < 0 ? 'text-danger' : 'text-success'
+    }
+  },
+
+  methods: {
+    round (val, places = 2) {
+      return Math.round(val * 10 * places) / (places * 10)
     }
   }
 }
