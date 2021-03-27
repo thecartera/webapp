31e38be957539949ff007f928fcd76a521156122
Joao Meyer <1994meyer@gmail.com>
2021-03-26 23:51:15 -0300

    Simplified

--- src/components/Wallet.vue
+++ src/components/Wallet.vue
@@ -3,20 +3,22 @@
     <b-card-title>
       <b-row>
         <b-col>
-            <h5 :class="darkGray">
-              Rendimento da carteira em 30 dias:
-              <span :class="gainColor"> {{ round(wallet.gain30d) }}% </span>
-            </h5>
+          <h5 class="text-dark">
+            Rendimento da carteira em 30 dias:
+            <span :class="gainColor"> {{ wallet.gain30d.toFixed(2) }}% </span>
+          </h5>
+        </b-col>
+      </b-row>
+
+      <b-row>
+        <b-col>
+          <h6 class="text-secondary"> ID desta carteira: {{ wallet.id }} </h6>
         </b-col>
       </b-row>
       <b-row>
+
         <b-col>
-          <h6 :class="lightGray"> ID desta carteira: {{ wallet.id }} </h6>
-        </b-col>
-      </b-row>
-      <b-row>
-        <b-col>
-          <h6 :class="lightGray"> Criada em: {{ created }} </h6>
+          <h6 class="text-secondary"> Criada em: {{ created }} </h6>
         </b-col>
       </b-row>
     </b-card-title>
@@ -25,31 +27,34 @@
       <b-table responsive='lg' hover :fields="fields" :items="wallet.assets">
         <template #cell(index)="data" >
           <p></p>
-          <p style="font-family:'Courier New'"> {{ data.index + 1 }} </p>
+          <p> {{ data.index + 1 }} </p>
         </template>
+
         <template #cell(imageLink)="data">
-          <b-avatar :src="getImageLink(data)" size="3.2em" icon="wallet2" variant="info"></b-avatar>
+          <b-avatar :src="getImageLink(data)" size="3.2em" icon="wallet2" variant="info" />
         </template>
+
         <template #cell(nameticker)="data">
-          <span style="font-size:0.8rem;color:gray"> {{ data.item.name }} </span>
-          <p style="font-family:'Courier New';font-size:1.2rem"> {{ data.item.ticker.toUpperCase() }} </p>
+          <span class="cell-name"> {{ data.item.name }} </span>
+          <p class="cell-value"> {{ data.item.ticker.toUpperCase() }} </p>
         </template>
+
         <template #cell(weight)="data">
-          <span style="font-size:0.8rem;color:gray">Peso</span>
-          <p style="font-family:'Courier New';font-size:1.2rem;color:#0275B1">{{ round(data.value) }}%</p>
+          <span class="cell-name"> Peso </span>
+          <p class="light-blue cell-value"> {{ data.value.toFixed(2) }}% </p>
         </template>
+
         <template #cell(price)="data">
-          <span style="font-size:0.8rem;color:gray">Preço Atual</span>
-          <p style="font-family:'Courier New';font-size:1.2rem;color:#0275B1"> R$ {{ round(data.value) }} </p>
+          <span class="cell-name"> Preço Atual </span>
+          <p class="light-blue cell-value"> R$ {{ data.value.toFixed(2) }} </p>
         </template>
+
         <template #cell(gain30d)="data">
-          <span style="font-size:0.8rem;color:gray">Retorno (30 dias)</span>
-          <p :class="positive(data.value)" style="font-family:'Courier New';font-size:1.2rem"> {{ round(data.value) }}% </p>
+          <span class="cell-name"> Retorno (30 dias) </span>
+          <p :class="positive(data.value)" class="cell-value"> {{ data.value.toFixed(2) }}% </p>
         </template>
       </b-table>
     </b-card-body>
-    <!-- <b-icon-share-fill @click="copyLink"></b-icon-share-fill> -->
-    <!-- <span> Copiar Link </span> -->
   </b-card>
 </template>
 
@@ -82,38 +87,36 @@ export default {
     },
 
     gainColor () {
-      return this.wallet.gain30d < 0 ? 'text-danger' : 'text-success'
-    },
-
-    darkGray () {
-      return 'text-dark'
-    },
-
-    lightGray () {
-      return 'text-secondary'
+      return this.positive(this.wallet.gain30d)
     }
   },
+
   methods: {
-    round (val, places = 2) {
-      const decimals = Math.pow(10, places)
-      const rounded = Math.round(val * decimals) / decimals
-      // console.log(data)
-      return rounded.toFixed(2)
-    },
-
-    copyLink () {
-      console.log('todo')
-    },
-
     positive (value) {
       return value < 0 ? 'text-danger' : 'text-success'
     },
 
     getImageLink (data) {
       const code = data.item.ticker.toUpperCase()
-      const imageLink = `https://raw.githubusercontent.com/monneda/B3-Assets-Images/main/imgs/${code}.png`
-      return imageLink
+      return `https://raw.githubusercontent.com/monneda/B3-Assets-Images/main/imgs/${code}.png`
     }
   }
 }
 </script>
+<style scoped>
+
+.cell-value {
+  font-family: 'Courier New';
+  font-size: 1.2rem;
+}
+
+.cell-name {
+  font-size: 0.8rem;
+  color: gray;
+}
+
+.light-blue {
+  color: #0275B1;
+}
+
+</style>
