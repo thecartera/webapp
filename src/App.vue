4fa4cba8a36555b11f894504510afe63ba9e5dbd
G M Bueno <guilherme.bueno@leafagriculture.com.br>
2021-03-30 18:21:04 -0300

    better ui, even more so on mobile

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <b-container>
-    <router-view />
-  </b-container>
+<!-- if want to use the entire screen, change "container" to "container-fluid" -->
+<!-- px-0 is used to take advantage of the full width (no margins/padding) of the screen -->
+  <div class="container px-0">
+      <router-view />
+  </div>
 </template>
