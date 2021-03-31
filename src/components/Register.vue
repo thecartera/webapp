5a5803e248b74653ad57ded6436d196d4c2e2822
Mauricio Barbosa <mauriciombarbosa@hotmail.com>
2021-03-31 13:18:39 -0300

    Fixing validUsername logic

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -190,13 +190,13 @@ export default {
       const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
       const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
       const cLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+      if (this.form.username.length < 20) { return false }
       for (const key of this.form.username) {
-        if ((letters.includes(key) || cLetters.includes(key) || key in numbers) && this.form.username.length < 20) {
-          return true
-        } else {
+        if (!(letters.includes(key) || cLetters.includes(key) || key in numbers)) {
           return false
         }
       }
+      return true
     },
 
     allInputsAreValid () {
