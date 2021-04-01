f5420629b65749a027f4725698a58118f1fc1910
Mauricio Barbosa <mauriciombarbosa@hotmail.com>
2021-03-31 23:36:59 -0300

    adding audience to auth0 plugin

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import './plugins/bootstrap-vue'
 import router from './router'
 
 // Import the Auth0 configuration
-import { domain, clientId } from '../auth_config.json'
+import { domain, clientId, audience } from '../auth_config.json'
 
 // Import the plugin here
 import { Auth0Plugin } from './auth'
@@ -18,6 +18,7 @@ import { Auth0Plugin } from './auth'
 Vue.use(Auth0Plugin, {
   domain,
   clientId,
+  audience,
   onRedirectCallback: appState => {
     router.push(
       appState && appState.targetUrl
