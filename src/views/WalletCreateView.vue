6e685399fd4cbef9eacf6f1145331599aaf5f8e9
Joao Meyer <1994meyer@gmail.com>
2021-04-18 15:34:19 -0300

    Simplified walletcreate

---
+++ src/views/WalletCreateView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Navbar />
+    <WalletCreate />
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar'
+import WalletCreate from '@/components/WalletCreate'
+
+export default {
+  name: 'WalletCreateView',
+
+  components: {
+    Navbar,
+    WalletCreate
+  }
+}
+</script>
