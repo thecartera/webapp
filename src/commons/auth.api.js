eeca45343b69b029f12978ba4665118eff7b4787
Joao Meyer <1994meyer@gmail.com>
2021-04-21 16:53:31 -0300

    Now we use the correct id

--- src/commons/auth.api.js
+++ src/commons/auth.api.js
@@ -1,9 +1,16 @@
 import { Auth0Client } from '@auth0/auth0-spa-js'
 
+// See here for more info on this variable:
+//    https://cli.vuejs.org/guide/mode-and-env.html#modes
+let clientId = 'wXUXvTKAbog7si5kiaP5Be8Td23XOtRC' // dev
+if (process.env.NODE_ENV === 'production') {
+  clientId = 'moxpPIgMLGr5LEfOyr3NB0ja4iVsQP1L' // prd
+}
+
 const auth0 = new Auth0Client({
   domain: 'monneda.us.auth0.com',
   audience: 'https://api.monneda.com',
-  client_id: 'moxpPIgMLGr5LEfOyr3NB0ja4iVsQP1L',
+  client_id: clientId,
   redirect_uri: `${window.location.origin}/#/callback`,
   cacheLocation: 'localstorage'
 })
