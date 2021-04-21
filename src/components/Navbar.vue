fd650027fff30b26076bdcda10b09fe977fea2b2
Joao Meyer <1994meyer@gmail.com>
2021-04-21 17:35:37 -0300

    Simplified some things

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,52 +1,51 @@
 <template>
-  <b-navbar class="navbarsize" sticky toggleable="sm" type="dark" variant="info">
+  <b-navbar sticky toggleable="sm" variant="info">
     <!-- Homme button -->
     <b-navbar-brand to="/">
-      <img src="@/assets/logo40x40.png" alt="Monneda">
+      <img src="@/assets/logo40x40.png" alt="Monneda" />
     </b-navbar-brand>
 
     <!-- Search bar -->
     <b-navbar-nav>
-      <b-input-group size="sm" prepend="@">
+      <b-input-group prepend="@">
         <b-form-input
-          class="mr-sm-2"
           placeholder="Username"
           @keypress.enter.prevent="search"
           v-model="searchBarValue"
-          size="sm">
+        >
         </b-form-input>
       </b-input-group>
     </b-navbar-nav>
 
+    <!-- Toggle navbar button -->
     <b-navbar-toggle target="collapse" />
-    <b-collapse id="collapse" is-nav>
 
+    <!-- All items of navbar -->
+    <b-collapse id="collapse" is-nav>
       <b-navbar-nav class="ml-auto">
         <!-- Create wallet button -->
         <b-nav-item to="/wallets">
-          <b-button block size="sm" variant="info">
-            <b-iconstack>
-              <b-icon variant="light" icon="wallet2" shift-h="-2" shift-v="2" />
-              <b-icon variant="light" icon="plus" shift-v="1" shift-h="-2" />
+          <b-button variant="info">
+            <b-iconstack variant="light">
+              <b-icon icon="wallet2" shift-h="-2" shift-v="2" />
+              <b-icon icon="plus" shift-v="1" shift-h="-2" />
             </b-iconstack>
           </b-button>
         </b-nav-item>
 
         <!-- Login/logout buttons -->
         <b-nav-item>
-          <b-button block size="sm" variant="info" v-if="auth" @click="logout">
+          <b-button variant="info" v-if="auth" @click="logout">
             Sair
           </b-button>
-          <b-button block size="sm" variant="info" v-else @click="login">
+          <b-button variant="info" v-else @click="login">
             Entrar
           </b-button>
         </b-nav-item>
 
         <!-- Profile button -->
-        <b-nav-item v-if="auth" :to="`/users/${this.user.nickname}`">
-          <div class="row justify-content-center">
-            <b-avatar :src="user.picture" style="width: 2rem; height: 2rem"></b-avatar>
-          </div>
+        <b-nav-item v-if="auth" :to="this.userUrl">
+          <b-avatar :src="user.picture" />
         </b-nav-item>
       </b-navbar-nav>
     </b-collapse>
@@ -69,6 +68,9 @@ export default {
     },
     auth () {
       return this.$store.state.auth.auth
+    },
+    userUrl () {
+      return `/users/${this.user.nickname}`
     }
   },
 
