e30557cbce0d333ac5b312f0411471dd30068f40
Maurício M <mauriciombarbosa@hotmail.com>
2021-04-28 13:21:53 -0300

    Implementing instant search

--- src/main.js
+++ src/main.js
@@ -9,10 +9,15 @@ import Vue from 'vue'
 import './plugins/bootstrap'
 import 'chartjs-plugin-colorschemes'
 
+// MeiliSearch
+import InstantSearch from 'vue-instantsearch'
+
 // App itself
 import App from './App.vue'
 import store from './store'
 import router from './router'
+
+Vue.use(InstantSearch);
 
 Vue.config.productionTip = false
 
