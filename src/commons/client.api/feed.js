5e0fac606d1390aac916fdf24a7d1275cb91a7ab
Maurício M <mauriciombarbosa@hotmail.com>
2021-05-18 20:09:41 -0300

    getting feed by size and before_id

--- src/commons/client.api/feed.js
+++ src/commons/client.api/feed.js
@@ -1,7 +1,13 @@
 import Client from './client'
 
 export default class extends Client {
-  getFeed () {
-    return this.request('GET', 'feed')
+  getFeed (size, beforeId) {
+    if (beforeId && size) {
+      return this.request('GET', `feed?size=${size}&before_id=${beforeId}`)
+    } else if (size) {
+      return this.request('GET', `feed?size=${size}`)
+    } else {
+      return this.request('GET', 'feed?size=20')
+    }
   }
 }
