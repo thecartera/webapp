36eab4e18b952d7bf91969452b572621db768b21
gmbueno <gmbueno1802@gmail.com>
2021-05-21 22:54:18 -0300

    redesigned portfolio info card

--- src/components/wallet/Info.vue
+++ src/components/wallet/Info.vue
@@ -1,46 +1,76 @@
 <template>
-<b-card no-body style="padding: 1rem 0.7rem 0rem 0.7rem">
-  <b-card-title>
+<b-card no-body>
+  <b-card-header class="pt-1 pb-1 m-0">
     <b-row>
-      <!-- title Cartera -->
-      <b-col style="line-height: 1.3rem">
-        <span class="cartera-title"> Cartera </span>
-        <br>
-        <b-link class="h6" :to="`/users/${wallet.username}`">
-          @{{ wallet.username }}
-        </b-link>
+      <b-col cols="auto" class="pl-0 pr-0">
+        <b-avatar
+          rounded
+          :src="ownerImage"
+          size="md"
+          :to="`/users/${wallet.username}`"
+        />
       </b-col>
-      <!-- Image and location -->
+      <b-col class="pl-3" style="line-height:1.35rem">
+        <b-row>
+          <b-link style="color: gray" class="pl-1" :to="`/users/${wallet.username}`">
+            {{ wallet.username }}
+          </b-link>
+        </b-row>
+        <b-row>
+          <!-- title Cartera -->
+          <span class="cartera-title pl-1"> Cartera </span>
+        </b-row>
+      </b-col>
       <b-col>
-        <b-row align-h="end">
-          <b-col cols="auto">
-            <b-avatar
-              rounded
-              :src="ownerImage"
-              size="3.5rem"
-              :to="`/users/${wallet.username}`"
+        <b-row align-h="end" class="pt-1 pr-1">
+          <b-dropdown no-caret right variant="outline-light">
+            <template #button-content>
+              <b-icon
+                icon="three-dots-vertical"
+                scale="1"
+                style="cursor: pointer"
+                variant="dark"
               />
-          </b-col>
+            </template>
+            <b-dropdown-item-button
+              style="width:15rem"
+              v-if="wallet.username === user.username"
+              @click="gotoWalletEditor">
+              <b-icon
+                scale="1"
+                icon="pencil-square"
+                variant="secondary"
+                style="cursor: pointer"
+              /> Editar carteira
+            </b-dropdown-item-button>
+            <b-dropdown-item-button>
+              <vue-goodshare-whats-app style="width: 12rem" title_social="Enviar no Whats" has_icon/>
+            </b-dropdown-item-button>
+            <b-dropdown-item-button>
+              <vue-goodshare-telegram style="width: 12rem" title_social="Enviar no Telegram" has_icon/>
+            </b-dropdown-item-button>
+            <b-dropdown-item-button>
+              <vue-goodshare-twitter style="width: 12rem" title_social="Enviar no Twitter" has_icon/>
+            </b-dropdown-item-button>
+            <b-dropdown-item-button>
+              <vue-goodshare-facebook style="width: 12rem" title_social="Enviar no Facebook" has_icon/>
+            </b-dropdown-item-button>
+          </b-dropdown>
         </b-row>
       </b-col>
     </b-row>
-  </b-card-title>
+  </b-card-header>
 
-  <b-card-body style="padding: 0rem 0rem 0rem 0rem">
+  <b-card-body class="pl-2 pt-2 pb-0">
     <!-- PORTFOLIO NAME AND EDIT BUTTON -->
     <b-row>
       <!-- LEFT COLUMN: Portfolio name -->
-      <b-col class="h3">
+      <b-col>
+        <h4>
         {{ wallet.name }}
+        </h4>
       </b-col>
       <!-- RIGHT COLUMN: Edit button -->
-      <b-col cols="auto">
-        <b-row align-h="end" style="margin-right: auto">
-          <b-button v-if="wallet.username === user.username" variant="outline-secondary" @click="gotoWalletEditor">
-            <b-icon icon="pencil"/>
-          </b-button>
-        </b-row>
-      </b-col>
     </b-row>
 
     <!-- DESCRIPTION -->
@@ -53,7 +83,7 @@
 
     <b-row>
     <!-- Wallet info -->
-      <b-col class="h5" style="margin-top: 1rem">
+      <b-col class="h6" style="margin-top: 1rem">
         <dl>
           <b-row style="margin-left: auto"> Retorno (30d)
             <b-icon icon="question-circle-fill"
@@ -71,10 +101,15 @@
               Em relação aos preços de fechamento de ontem e 30 dias antes
             </b-tooltip>
           </b-row>
-          <b-row align-h="start" :class="positive(wallet.gain)" style="margin-left: 1.5rem">
-            {{ wallet.gain ? wallet.gain.toFixed(2) : 0.0 }}%
-            <b-icon v-if="wallet.gain<0" icon="arrow-down"/>
-            <b-icon v-else icon="arrow-up"/>
+          <b-row class="mt-1 ml-2" align-v="center">
+              <b-iconstack shift-v="1">
+                <b-icon scale="1.2" stacked icon="circle" :variant="wallet.gain < 0? 'danger' : 'success'"/>
+                <b-icon v-if="wallet.gain < 0" stacked icon="arrow-down" variant="danger"/>
+                <b-icon v-else stacked icon="arrow-up" variant="success"/>
+              </b-iconstack>
+              <span class="pl-2 mr-2" style="font-size: 1rem">
+                {{ wallet.gain ? wallet.gain.toFixed(2) : 0.0 }}%
+              </span>
           </b-row>
         </dl>
       </b-col>
@@ -82,9 +117,9 @@
       <!-- Share buttons -->
       <b-col align-self="end" cols="auto">
         <b-row style="margin-bottom: 2rem; margin-right: auto">
-          <b-dropdown right variant="outline-info" no-caret>
+          <b-dropdown right variant="outline-light" no-caret>
             <template #button-content>
-              Compartilhar
+              <b-img class="mt-3" width="24rem" height="24rem" src="../../assets/share.svg"/>
               <!-- <b-icon variant="info" icon="share-fill" /> -->
             </template>
             <b-dropdown-item-button>
@@ -170,7 +205,7 @@ export default {
 
 .cartera-title {
   font-family: 'Helvetica';
-  font-size: 2rem;
+  font-size: 1rem;
   color: #33d260;
   font-weight: bold;
 }
