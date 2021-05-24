b7fc3b4127bb15f4c71f1be7d276c3744e512518
gmbueno <gmbueno1802@gmail.com>
2021-05-24 18:47:18 -0300

    test img upload

---
+++ src/components/testimg.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <input type="file" @change="onFileChanged"/>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      selectedFile: null
+    }
+  },
+
+  methods: {
+    onFileChanged (event) {
+      const file = event.target.files[0]
+      console.log(file)
+    }
+  }
+}
+
+</script>
