dc5f0a6aeb4d708a93677a4a7cfe4af108d4b009
Maurício M <mauriciombarbosa@hotmail.com>
2021-05-24 11:52:27 -0300

    fetching feed paginated

--- src/views/FeedView.vue
+++ src/views/FeedView.vue
@@ -13,6 +13,7 @@
               <FeedItem :item="i" />
             </b-col>
           </b-row>
+          <b-badge style="color:#F3F2EF; background-color:#F3F2EF" v-if="this.feed.length" v-b-visible="loadFeedItems">.</b-badge>
         </b-col>
 
         <b-col cols="0" md="4" lg="3" />
@@ -39,8 +40,16 @@ export default {
     feed: []
   }),
 
+  methods: {
+    async loadFeedItems () {
+      const lastId = this.feed[this.feed.length - 1].id
+      const newItems = await client.feed.getFeed(20, lastId)
+      this.feed.push.apply(this.feed, newItems)
+    }
+  },
+
   async mounted () {
-    this.feed = await client.feed.getFeed(20)
+    this.feed = await client.feed.getFeed(10)
   }
 }
 </script>
