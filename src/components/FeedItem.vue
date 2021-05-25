2ff92f415fada1edb84092127f8a00d1acb96411
gmbueno <gmbueno1802@gmail.com>
2021-05-24 22:49:25 -0300

    styling cards on feed

--- src/components/FeedItem.vue
+++ src/components/FeedItem.vue
@@ -32,35 +32,37 @@
     </b-row>
 
     <b-row class="pt-2 mb-2" v-if="buys.length > 0">
-      <b-col cols="auto" align-self="center" class="text-success">
+      <b-col cols="auto" style="width:6.5rem" align-self="center" class="text-success">
         Compras:
       </b-col>
       <b-avatar
+        icon="wallet"
+        variant="success"
         rounded
         size="2.2rem"
         class="mr-1"
-        v-for="event of buys.slice(0, 5)"
+        v-for="event of buys.slice(0, 4)"
         :key="event.ticker"
         :src="thumb(event.ticker)"
       >
       </b-avatar>
-      <b-avatar size="2.2rem" rounded v-if="buys.length >= 5" icon="three-dots" variant="info"/>
+      <b-avatar size="2.2rem" rounded v-if="buys.length >= 4" icon="three-dots" variant="primary"/>
     </b-row>
 
     <b-row class="pt-2 mb-2" v-if="sells.length > 0">
-      <b-col cols="auto" align-self="center" class="text-danger">
+      <b-col cols="auto" style="width:6.5rem" align-self="center" class="text-danger">
         Vendas:
       </b-col>
       <b-avatar
         rounded
         size="2.2rem"
         class="mr-1"
-        v-for="event of sells.slice(0, 5)"
+        v-for="event of sells.slice(0, 4)"
         :key="event.ticker"
         :src="thumb(event.ticker)"
       >
       </b-avatar>
-      <b-avatar size="2.2rem" rounded v-if="sells.length >= 5" icon="three-dots" variant="info"/>
+      <b-avatar size="2.2rem" rounded v-if="sells.length >= 4" icon="three-dots" variant="primary"/>
     </b-row>
 
     <b-card-footer class="pt-0 m-0 px-3 pb-0" footer-bg-variant="white">
