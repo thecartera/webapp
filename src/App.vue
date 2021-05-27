ce5ee159c6b2ddebca11bf9d391568fdbc22924c
gmbueno <gmbueno1802@gmail.com>
2021-05-27 14:56:26 -0300

    removed style='overflow-x: hidden' bc made the navbar not sticky. Trying with max-width: 100%  now because we don't want horizontal scroll, specially on mobile

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
 <!-- if want to use the entire screen, change "container" to "container-fluid" -->
 <!-- px-0 is used to take advantage of the full width (no margins/padding) of the screen -->
-  <div class="container-fluid px-0" style="overflow-x: hidden">
+  <div class="container-fluid px-0" style="max-width: 100%">
       <router-view />
   </div>
 </template>
