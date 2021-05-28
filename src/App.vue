bc02d6c04259a02fb3902193da862b75f0e3115f
gmbueno <gmbueno1802@gmail.com>
2021-05-28 15:19:58 -0300

    using bootstrap class to get 100vh

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
 <!-- if want to use the entire screen, change "container" to "container-fluid" -->
 <!-- px-0 is used to take advantage of the full width (no margins/padding) of the screen -->
-  <div class="container-fluid px-0" style="min-height: 100vh">
+  <div class="container-fluid px-0 min-vh-100">
       <router-view />
   </div>
 </template>
