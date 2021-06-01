206ffaced6533018fba269484ef1b5a754f61fb2
Maurício M <mauriciombarbosa@hotmail.com>
2021-06-01 15:03:23 -0300

    Changing tenant auth0

--- src/commons/auth.api.js
+++ src/commons/auth.api.js
@@ -2,14 +2,14 @@ import { Auth0Client } from '@auth0/auth0-spa-js'
 
 // See here for more info on this variable:
 //    https://cli.vuejs.org/guide/mode-and-env.html#modes
-let clientId = 'wXUXvTKAbog7si5kiaP5Be8Td23XOtRC' // dev
+let clientId = 'LMFDlOJhgdnFEvXK45rcd3M489fXnYFU' // dev
 if (process.env.NODE_ENV === 'production') {
-  clientId = 'moxpPIgMLGr5LEfOyr3NB0ja4iVsQP1L' // prd
+  clientId = 'rD7cai4xl9436V3RPJvGC7lQpe6HQQfR' // prd
 }
 
 const auth0 = new Auth0Client({
-  domain: 'monneda.us.auth0.com',
-  audience: 'https://api.monneda.com',
+  domain: 'cartera.us.auth0.com',
+  audience: 'https://api.cartera.com',
   client_id: clientId,
   redirect_uri: `${window.location.origin}/#/callback`,
   cacheLocation: 'localstorage'
