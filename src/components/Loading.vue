7f4ec3885422004367b78ba7ad310b15aea8237f
gmbueno <gmbueno1802@gmail.com>
2021-06-04 15:43:05 -0300

    added a Loading (spinning arrow) component to be reused across webapp

---
+++ src/components/Loading.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-row align-v="center" class="vh-80" align-h="center">
+    <b-icon
+      variant="primary"
+      icon="arrow-clockwise"
+      animation="spin"
+      font-scale="4"
+    />
+  </b-row>
+</template>
+
+<script>
+
+export default {
+  name: 'Loading',
+
+  data: () => ({
+    loading: true
+  })
+}
+</script>
+
+<style scoped>
+.vh-80 {
+  height: 80vh;
+}
+</style>
