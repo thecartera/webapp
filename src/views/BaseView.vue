2f70a3c379e8e27ce0184aa74a91693d34106b80
Joao Meyer <1994meyer@gmail.com>
2021-06-06 13:39:06 -0300

    Removed footer and navbar from view

--- src/views/BaseView.vue
+++ src/views/BaseView.vue
@@ -1,9 +1,7 @@
 <template>
-<div class="d-flex flex-column justify-content-between vh-100">
-  <Navbar v-if="navbar"/>
-
+<div class="d-flex flex-column justify-content-between">
   <div class="d-flex justify-content-between h-100 p-0">
-    <div class="d-none d-md-block col-0 col-md-3">
+    <div class="d-none d-md-block col-md-3">
       <slot name="left"></slot>
     </div>
 
@@ -11,34 +9,9 @@
       <slot></slot>
     </div>
 
-    <div class="d-none d-md-block col-0 col-md-3">
+    <div class="d-none d-md-block col-md-3">
       <slot name="right"></slot>
     </div>
   </div>
-
-  <Footer v-if="footer" class="align-self-end" />
 </div>
 </template>
-
-<script>
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
-
-export default {
-  components: {
-    Navbar,
-    Footer
-  },
-
-  props: {
-    footer: {
-      type: Boolean,
-      default: false
-    },
-    navbar: {
-      type: Boolean,
-      default: false
-    }
-  }
-}
-</script>
