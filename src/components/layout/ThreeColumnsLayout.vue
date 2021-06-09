9857dfce127a5eb64d8fe373f738bf78d57d0a40
gmbueno <gmbueno1802@gmail.com>
2021-06-09 14:06:46 -0300

    minor fix on padding (was (implicitly) too big)

--- src/components/layout/ThreeColumnsLayout.vue
+++ src/components/layout/ThreeColumnsLayout.vue
@@ -1,15 +1,15 @@
 <template>
 <div class="d-flex flex-column justify-content-between">
-  <div class="d-flex justify-content-between h-100 p-0">
-    <div class="d-none d-md-block col-md-3">
+  <div class="d-flex justify-content-between h-100">
+    <div class="d-none d-md-block col-md-3 p-1">
       <slot name="left"></slot>
     </div>
 
-    <div class="col-12 col-md-6">
+    <div class="col-12 col-md-6 p-1">
       <slot></slot>
     </div>
 
-    <div class="d-none d-md-block col-md-3">
+    <div class="d-none d-md-block col-md-3 p-1">
       <slot name="right"></slot>
     </div>
   </div>
