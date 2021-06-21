6c2ac4267035695158f52636ee26a01d676bee79
Maurício M <mauriciombarbosa@hotmail.com>
2021-06-21 13:51:03 -0300

    Creating hyperlinks in posts if contains urls

--- src/components/posts/PostEventItem.vue
+++ src/components/posts/PostEventItem.vue
@@ -3,13 +3,15 @@
     <!-- Event -->
     <b-row class="p-2">
       <b-col>
-        {{ item.data.text }}
+        <span style="white-space: pre-wrap" class="text-break" v-html="linkifiedText">  </span>
       </b-col>
     </b-row>
   </div>
 </template>
 
 <script>
+import client from '@/commons/client.api'
+
 export default {
   name: 'PostEventItem',
   props: {
@@ -17,6 +19,13 @@ export default {
       type: Object,
       required: true
     }
+  },
+  data: () => ({
+    linkifiedText: ''
+  }),
+
+  async created () {
+    this.linkifiedText = await client.utils.linkify(this.item.data.text)
   }
 }
 </script>
