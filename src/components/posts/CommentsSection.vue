42a09a5d06350e3a468779bbebc2f6ec91bb2e6d
Joao Meyer <1994meyer@gmail.com>
2021-06-22 21:56:52 -0300

    Fixed conflict

--- src/components/posts/CommentsSection.vue
+++ src/components/posts/CommentsSection.vue
@@ -1,81 +1,35 @@
 <template>
-  <b-card no-body class="px-3" border-variant="white">
-    <b-row class="mt-2" align-h="start" v-for="comment of comments.slice(0, visibleQtty)" :key="comment.id">
-      <b-col class="px-0 pt-1" cols="auto" align-self="start">
-        <b-avatar :src="comment.user.picture" size="2rem" :to="`/users/${comment.user.username}`"/>
-      </b-col>
-      <b-col class="my-0 p-0" align-self="start">
-        <b-card bg-variant="cartera-blue" class="ml-2 py-1" no-body>
-          <b-row class="mx-0">
-            <b-col class="pl-4" style="line-height: 1rem">
-              <b-row align-h="start">
-                <b-link
-                  :to="`/users/${comment.user.username}`"
-                  style="font-size: 0.9rem"
-                  class="font-weight-bold text-dark"
-                >
-                    {{ comment.user.username }}
-                </b-link>
-                <span class="small text-secondary pl-2"> {{ humanTime(comment.createdAt) }} </span>
-              </b-row>
-              <b-row class="px-0 small text-secondary">
-                <span class="text-wrap"> {{ comment.user.title }} </span>
-              </b-row>
-            </b-col>
-            <b-col cols="auto" class="mr-3 p-0" v-if="loggedUser.username === comment.user.username">
-              <b-row align-h="end">
-                <b-dropdown no-caret size="sm" right variant="cartera-blue">
-                  <template #button-content>
-                    <b-icon
-                      icon="three-dots-vertical"
-                      scale="0.7"
-                      style="cursor: pointer"
-                      variant="dark"
-                    />
-                  </template>
+<b-container>
+  <!-- Comments -->
+  <template v-for="comment of sliced">
+    <Comment
+      class="my-2"
+      :key="comment.id"
+      :comment="comment"
+      @delete="deleteComment"
+    />
+  </template>
 
-                <!-- Delete Comment -->
-                <b-dropdown-item-button
-                  v-if="loggedUser.username === comment.user.username"
-                  @click="deleteComment(comment.id)"
-                >
-                  <b-icon
-                    icon="trash"
-                    variant="secondary"
-                  /> <span> Excluir comentário </span>
-                </b-dropdown-item-button>
-                </b-dropdown>
-              </b-row>
-            </b-col>
-          </b-row>
-          <b-row class="px-4 pt-1" style="line-height: 1.2rem">
-            <span class="text-break"> {{comment.text}} </span>
-          </b-row>
-        </b-card>
-      </b-col>
-    </b-row>
-    <b-row align-h="center" class="mb-2">
-      <b-col cols="auto">
-        <b-container
-          v-if="visibleQtty < commentCount && comments.length > visibleQtty"
-          class="mt-2"
-          @click="loadMoreComments">
-          <span class="text-bold text-primary show-clickable"> Ver mais </span>
-        </b-container>
-      </b-col>
-      <b-col v-if="visibleQtty != 3" cols="auto">
-        <b-container @click="visibleQtty = 3" class="mt-2">
-          <span class="text-bold text-primary show-clickable"> Ver menos </span>
-        </b-container>
-      </b-col>
-    </b-row>
-  </b-card>
+  <!-- See more button -->
+  <b-row align-h="center" class="my-2">
+    <b-col cols="auto" v-if="canShowLess">
+      <b-link @click="showLess"> Ver menos </b-link>
+    </b-col>
+    <b-col cols="auto" v-if="canShowMore">
+      <b-link @click="showMore"> Ver mais </b-link>
+    </b-col>
+  </b-row>
+</b-container>
 </template>
 
 <script>
-import client from '@/commons/client.api'
+import Comment from '@/components/posts/Comment'
 
 export default {
+  components: {
+    Comment
+  },
+
   props: {
     comments: [],
     commentCount: Number
@@ -87,29 +41,34 @@ export default {
   }),
 
   computed: {
-    loggedUser () {
-      return this.$store.state.auth.user
+    sliced () {
+      return this.comments.slice(0, this.visibleQtty)
+    },
+
+    canShowMore () {
+      return this.visibleQtty < this.commentCount &&
+        this.comments.length > this.visibleQtty
+    },
+
+    canShowLess () {
+      return this.visibleQtty !== 3
     }
   },
 
   methods: {
-    loadMoreComments () {
+    showMore () {
       this.visibleQtty += this.step
       const beforeId = this.comments[this.comments.length - 1].id
       this.$emit('load-more-comments', beforeId, this.step)
     },
-    deleteComment (commentId) {
-      this.$emit('delete-comment', commentId)
+
+    showLess () {
+      this.visibleQtty = 3
     },
-    humanTime (timestamp) {
-      return client.utils.humanTime(timestamp)
+
+    deleteComment ({ id }) {
+      this.$emit('delete-comment', id)
     }
   }
 }
 </script>
-
-<style scoped>
-.show-clickable {
-  cursor: pointer
-}
-</style>
