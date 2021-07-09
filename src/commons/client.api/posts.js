1cfe194aac476a6a2b14d19fee3846db3825239a
Maurício M <mauriciombarbosa@hotmail.com>
2021-07-09 20:45:43 -0300

    adding pictures to posts

--- src/commons/client.api/posts.js
+++ src/commons/client.api/posts.js
@@ -1,9 +1,9 @@
 import Client from './client'
 
 export default class extends Client {
-  postPost (text) {
+  postPost (text, picture) {
     const options = {
-      body: JSON.stringify({ text: text }),
+      body: JSON.stringify({ text: text, picture: picture }),
       headers: { 'Content-Type': 'application/json' }
     }
     return this.request('POST', 'posts?type=text', options)
