fd673b564fd47c84060123037b31ee163986bb0e
Ramna Sidharta <ramna.thors@gmail.com>
2021-07-10 20:33:34 -0300

    Fix misalignment [💩]

--- src/components/utils/InviteDropdown.vue
+++ src/components/utils/InviteDropdown.vue
@@ -1,15 +1,22 @@
 <template>
+  <!-- Regarding the class binding in this b-nav-item-dropdown:
+   if we're not on mobile, then the dropdown is shown in the navbar (it's not collapsed), and in that case, if we don't
+   use class fixed-alignment, the dropdown button appears misaligned with the other buttons in the navbar. I bet there's
+   a simple, clean solution to this, but since I'm stupid on front-end styling, this is what I came up with.
+  -->
   <b-nav-item-dropdown title="Convidar amigo(a)"
                        no-caret dropleft
                        size="sm"
-                       variant="primary">
+                       variant="primary"
+                       :class="{'fixed-alignment': !isMobile}"
+  >
     <b-dropdown-header v-if="!isMobile" id="dropdown-header-label">
-      Convide um amigo
+      Convidar um amigo
     </b-dropdown-header>
 
     <template #button-content>
       <b-icon icon="person-plus" style="cursor: pointer" class="text-white"/>
-      <span v-if="isMobile" class="text-white"> Convidar amigo(a)</span>
+      <span class="text-white"> Convidar</span>
     </template>
 
     <Socials :url="`https://cartera.com.br`" />
@@ -36,5 +43,10 @@ export default {
     }
   }
 }
-
 </script>
+
+<style scoped>
+.fixed-alignment {
+  padding-top: 10px;
+}
+</style>
