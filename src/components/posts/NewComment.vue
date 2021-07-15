c5d4e22caeec7c751808ed0b521c8a9f1676139d
Ramna Sidharta <ramna.thors@gmail.com>
2021-07-14 23:36:43 -0300

    Prevent comments larger than the limit (300)

--- src/components/posts/NewComment.vue
+++ src/components/posts/NewComment.vue
@@ -5,18 +5,24 @@
         <b-avatar :src="loggedUser.picture" size="2rem"/>
       </b-col>
       <b-col class="pl-2 pr-0">
-        <b-input-group>
-          <b-form-input v-model="newCommentText" size="sm" placeholder="Adicione um comentário..."/>
-          <b-input-group-append>
-            <b-button
-              size="sm"
-              variant="primary"
-              @click="postComment(newCommentText)"
-            >
-              Publicar
-            </b-button>
-          </b-input-group-append>
-        </b-input-group>
+        <b-form-input v-model="newCommentText" size="sm" placeholder="Adicione um comentário..."/>
+      </b-col>
+    </b-row>
+    <b-row align-h="end" class="pt-2">
+      <b-col cols="auto" v-if="commentLength > 250">
+        <span :class="maxCommentLengthExceeded ? 'text-danger font-weight-bold' : ''">
+          {{ `${commentLength}/${maxCommentLength}` }}
+        </span>
+      </b-col>
+      <b-col cols="auto" class="p-0">
+        <b-button
+            size="sm"
+            variant="primary"
+            :disabled="maxCommentLengthExceeded"
+            @click="postComment(newCommentText)"
+        >
+          Publicar
+        </b-button>
       </b-col>
     </b-row>
   </b-card>
@@ -26,12 +32,23 @@
 
 export default {
   data: () => ({
-    newCommentText: ''
+    newCommentText: '',
+    maxCommentLength: 300
   }),
 
   computed: {
     loggedUser () {
       return this.$store.state.auth.user
+    },
+    commentLength () {
+      return this.newCommentText.length
+    },
+    showTextLimitHint () {
+      const lengthToShowLimitHint = this.commentLength - 50
+      return this.commentLength > lengthToShowLimitHint
+    },
+    maxCommentLengthExceeded () {
+      return this.commentLength > this.maxCommentLength
     }
   },
 
