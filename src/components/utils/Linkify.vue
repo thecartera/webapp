a42a27eea5f2308f411471eb0ce1e41de20c0d5d
Joao Meyer <1994meyer@gmail.com>
2021-07-18 18:50:42 -0300

    Added docs

--- src/components/utils/Linkify.vue
+++ src/components/utils/Linkify.vue
@@ -10,15 +10,44 @@
 </template>
 
 <script>
+/**
+ * Creates a list of items to be rendered.
+ *
+ * It will split the text and return a list
+ * of items in the format `{ text, tag }`.
+ *
+ * The tag will be `text` when the splits
+ * do not match the passed regex. Otherwise,
+ * it will be the passed tag.
+ */
 const split = (text, regex, tag) => {
   const splits = text.split(regex)
   const matches = Array.from(text.matchAll(regex))
 
   const results = []
+  // We iterate in 2s because when splitting a string,
+  // using grouped regex, it will include the group.
+  // For example:
+  //    > 'your nice string'.split(/n(ice)/)
+  //    [ 'your ', 'ice', ' string' ]
+  //    > 'your nice string'.split(/nice/)
+  //    [ 'your ', ' string' ]
+  //
+  // So, in order to ignore the grouped parts, we
+  // iterate in 2s.
   for (let i = 0; i < splits.length - 1; i += 2) {
     results.push({ text: splits[i], tag: 'text' })
+    // Because we are iterating in 2s, we need to divide
+    // the index by 2
     results.push({ text: matches[i / 2][1], tag: tag })
   }
+
+  // As we do not iterate till the end, we insert
+  // the last item here, manually.
+  // This is done because split will always have
+  // one more item than the `matchAll` result. In
+  // this case, it will have (2n + 1) items, because
+  // of the regex grouping explained above
   results.push({ text: splits[splits.length - 1], tag: 'text' })
 
   return results
@@ -40,8 +69,10 @@ export default {
       const mention = /@(\w+)/gim
       const http = /(https?:\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/gim
 
+      // Links
       let results = split(this.text, http, 'link')
 
+      // Cashtags
       for (let i = 0; i < results.length; i++) {
         const { text, tag } = results[i]
         if (tag === 'text') {
@@ -52,6 +83,7 @@ export default {
 
       results = results.flat()
 
+      // Mentions
       for (let i = 0; i < results.length; i++) {
         const { text, tag } = results[i]
         if (tag === 'text') {
