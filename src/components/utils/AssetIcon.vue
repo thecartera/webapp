fd632aa681f379a7e3a0b7451d7890ae3cc4243f
Joao Meyer <1994meyer@gmail.com>
2021-07-18 22:16:01 -0300

    Added AssetIcon component

---
+++ src/components/utils/AssetIcon.vue
@@ -0,0 +1,32 @@
+<template>
+<b-avatar
+  rounded
+  icon="wallet2"
+  variant="success"
+  responsive="true"
+  v-bind="$attrs"
+  :src="url"
+/>
+</template>
+
+<script>
+const BASE = 'https://raw.githubusercontent.com/monneda/B3-Assets-Images/main/imgs/'
+
+export default {
+  name: 'AssetIcon',
+  props: {
+    code: {
+      type: String,
+      default: null
+    }
+  },
+  computed: {
+    url () {
+      if (this.code) {
+        return BASE + `${this.code}.png`
+      }
+      return null
+    }
+  }
+}
+</script>
