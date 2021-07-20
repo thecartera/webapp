2819eeef65f0e6430704597f20168f473ae6dd8f
gmbueno <gmbueno1802@gmail.com>
2021-07-20 15:43:23 -0300

    custom msgs depending on notification type, also simplified code

--- src/commons/client.api/utils.js
+++ src/commons/client.api/utils.js
@@ -40,6 +40,57 @@ const humanTime = timestamp => {
   return 'há ' + months + (months === 1 ? ' mês' : ' meses')
 }
 
+const simpleHumanTime = timestamp => {
+  const diff = Date.now() - Date.parse(timestamp)
+  const second = 1000
+  const minute = 60 * second
+  const hour = 60 * minute
+  const day = 24 * hour
+  const week = 7 * day
+  const month = 30 * day
+  if (diff < second) {
+    return 'agora'
+  }
+  if (diff < minute) {
+    const seconds = Math.round(diff / second)
+    return seconds + 's'
+  }
+  if (diff < hour) {
+    const minutes = Math.round(diff / minute)
+    return minutes + 'min'
+  }
+  if (diff < day) {
+    const hours = Math.round(diff / hour)
+    return hours + 'h'
+  }
+  if (diff < week) {
+    const days = Math.round(diff / day)
+    return days + 'd'
+  }
+  if (diff < month) {
+    const weeks = Math.round(diff / week)
+    return weeks + 'sem'
+  }
+  const months = Math.round(diff / month)
+  return months + (months === 1 ? ' mês' : ' meses')
+}
+
+const linkify = inputText => {
+  // URLs starting with http://, https://, or ftp://
+  const replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/gim
+  let replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>')
+
+  // URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+  const replacePattern2 = /(^|[^/])(www\.[\S]+(\b|$))/gim
+  replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>')
+
+  // Change email addresses to mailto:: links.
+  const replacePattern3 = /(([a-zA-Z0-9\-_.])+@[a-zA-Z_]+?(\.[a-zA-Z]{2,6})+)/gim
+  replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>')
+
+  return replacedText
+}
+
 const extractUrls = inputText => {
   // URLs starting with http://, https://, or ftp://
   const pattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/gim
@@ -85,12 +136,46 @@ const getCode = ticker => {
   return ticker2code.ticker2code[ticker]
 }
 
+const generateNotificationText = notificationType => {
+  switch (notificationType) {
+    case 'USER_COMMENT_EVENT':
+      return 'comentou na sua postagem'
+    case 'USER_FOLLOWED':
+      return 'te seguiu'
+    case 'USER_LIKE_EVENT':
+      return 'curtiu sua postagem'
+    case 'USER_TAGGED_COMMENT_EVENT':
+      return 'te marcou em um comentário'
+    default:
+      return ': nova notificação'
+  }
+}
+
+const generateNotificationRedirectLink = notification => {
+  const notificationType = notification.type
+  switch (notificationType) {
+    case 'USER_COMMENT_EVENT':
+      return `/events/${notification.data.eventId}`
+    case 'USER_LIKE_EVENT':
+      return `/events/${notification.data.eventId}`
+    case 'USER_TAGGED_COMMENT_EVENT':
+      return `/events/${notification.data.eventId}`
+    case 'USER_FOLLOWED':
+      return `/users/${notification.user.username}`
+    default:
+      return ': nova notificação'
+  }
+}
+
 export default {
   thumbUrl,
   humanTime,
+  simpleHumanTime,
   extractUrls,
   getSector,
   getSubsector,
   getSegment,
-  getCode
+  getCode,
+  generateNotificationText,
+  generateNotificationRedirectLink
 }
--- src/components/navbars/subcomponents/Notifications.vue
+++ src/components/navbars/subcomponents/Notifications.vue
@@ -1,8 +1,7 @@
 <template>
     <b-nav-item-dropdown
       no-caret
-      :size="small ? 'sm' : 'md'"
-      title="Convidar amigo(a)"
+      size="sm"
       right
       variant="primary"
       class="pt-1 mx-1"
@@ -11,32 +10,30 @@
         <b-row align-h="center" class="m-0 p-0">
           <b-icon scale="1" variant="white" icon="bell"></b-icon>
         </b-row>
-        <b-row v-if="text" align-h="center" class="m-0 p-0">
+        <b-row align-h="center" class="m-0 p-0">
           <span class="text-white"> Notificações </span>
         </b-row>
       </template>
 
       <b-dropdown-item
-        :to="`/users/bueno`"
         v-for="notification in notifications"
         :key="notification.id"
         class="p-0 m-0"
       >
-        <b-row align-h="start">
-          <b-nav-item :to="`/users/bueno`">
-            <b-avatar :src="notification.user.picture" icon="person-circle" variant="dark"/>
-            <span class="text-dark"> {{ notification.user.name }} </span>
-            <span class="text-dark"> comentou no seu post </span>
-          </b-nav-item>
-        </b-row>
+        <Notification :notification="notification"/>
       </b-dropdown-item>
     </b-nav-item-dropdown>
 </template>
 
 <script>
+import Notification from '@/components/navbars/subcomponents/Notification'
 
 export default {
   name: 'Notifications',
+
+  components: {
+    Notification
+  },
 
   data: () => ({
     notifications: [
@@ -58,37 +55,54 @@ export default {
         }
       },
       {
-        id: '60f5ff2419a354187d3107b5',
-        createdAt: '2021-07-19T22:39:32.823Z',
+        id: '60f5c7d84e36790ece871c53',
+        createdAt: '2021-07-19T18:43:36.536Z',
         viewedAt: null,
         user: {
           name: 'Cartera',
           picture: 'https://lh3.googleusercontent.com/a-/AOh14Gj5JobFdjJ73FMYf5TxDDcMBEKEs4__x5amH4gU=s96-c',
           username: 'monnedanetwork',
           title: '',
-          following: false
+          following: true
         },
-        type: 'USER_TAGGED_COMMENT_EVENT',
+        type: 'USER_COMMENT_EVENT',
         data: {
-          eventId: '60f5dbf4a89e132aed1ee790',
-          text: 'Irado essa carteira eim.@Bueno, @Mauricio, @ramna@JOAO tá enviando notificação pra todo mundo'
+          eventId: '60f0482ef9e1973070108bb0',
+          text: 'irado'
+        }
+      },
+      {
+        id: '60f5be09abc3bb52cd53728c',
+        createdAt: '2021-07-19T18:01:45.917Z',
+        viewedAt: null,
+        user: {
+          name: 'Cartera',
+          picture: 'https://lh3.googleusercontent.com/a-/AOh14Gj5JobFdjJ73FMYf5TxDDcMBEKEs4__x5amH4gU=s96-c',
+          username: 'monnedanetwork',
+          title: '',
+          following: true
+        },
+        type: 'USER_FOLLOWED',
+        data: null
+      },
+      {
+        id: '60f5bc21abc3bb52cd537289',
+        createdAt: '2021-07-19T17:53:37.790Z',
+        viewedAt: null,
+        user: {
+          name: 'Cartera',
+          picture: 'https://lh3.googleusercontent.com/a-/AOh14Gj5JobFdjJ73FMYf5TxDDcMBEKEs4__x5amH4gU=s96-c',
+          username: 'monnedanetwork',
+          title: '',
+          following: true
+        },
+        type: 'USER_LIKE_EVENT',
+        data: {
+          eventId: '60f0482ef9e1973070108bb0'
         }
       }
     ]
-  }),
-
-  props: {
-    text: {
-      type: Boolean,
-      required: false,
-      default: true
-    },
-    small: {
-      type: Boolean,
-      required: false,
-      default: false
-    }
-  }
+  })
 
 }
 </script>
