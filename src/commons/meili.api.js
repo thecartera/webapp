00ebd230e2a9858f8210e8fc886b3e4ab4336c41
Maurício M <mauriciombarbosa@hotmail.com>
2021-07-31 14:30:35 -0300

    Meilisearch migration to heroku

--- src/commons/meili.api.js
+++ src/commons/meili.api.js
@@ -4,7 +4,7 @@ const options = {
   placeholderSearch: false
 }
 
-const url = 'https://meili-router-l3z1e5hoxqom1jdd-gtw.qovery.io/'
+const url = 'https://meilisearch-cartera.herokuapp.com/'
 const key = '9625a71bb5d6a90cee15060ed621a91f2f7fcb9e68c12a3e55918bd3c6029b24'
 
 export default instantMeiliSearch(url, key, options)
