0d943f9fee15a1ab6a26129d3beeff0128b224e8
Maurício M <mauriciombarbosa@hotmail.com>
2021-08-12 19:17:48 -0300

    fixing google analitics

--- src/plugins/vue-gtag.js
+++ src/plugins/vue-gtag.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueGtag from 'vue-gtag'
 
 Vue.use(VueGtag, {
-  config: { id: process.env.GA_MEASUREMENT_ID },
+  config: { id: 'G-Q9Z4RTC40B' },
   params: {
     send_page_view: false
   }
