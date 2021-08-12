752df68ff648a3fb45eefc651290f872e88de7c1
Maurício M <mauriciombarbosa@hotmail.com>
2021-08-12 20:00:54 -0300

    Adding router to google analytics. Now we have Views by Page

--- src/plugins/vue-gtag.js
+++ src/plugins/vue-gtag.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import VueGtag from 'vue-gtag'
+import router from './../router'
 
 Vue.use(VueGtag, {
   config: { id: 'G-Q9Z4RTC40B' },
   params: {
     send_page_view: false
   }
-})
+}, router)
