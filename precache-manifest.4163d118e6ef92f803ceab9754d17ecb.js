8a6832e2863e3250dc40a763ce5f07c858ece960
meyer1994 <meyer1994@users.noreply.github.com>
2021-10-27 14:01:33 +0000

    Deploying to gh-pages from @ monneda/webapp@0169b4c00f7c017a7ce218fed69c4274f304b0b0 🚀

---
+++ precache-manifest.4163d118e6ef92f803ceab9754d17ecb.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d243f3d8574662319935",
+    "url": "/css/app.0d75a57b.css"
+  },
+  {
+    "revision": "d94740290b42b991db44",
+    "url": "/css/chunk-vendors.af3221a8.css"
+  },
+  {
+    "revision": "9cea2f98108b16ed09dec91dc0e5a4d4",
+    "url": "/img/bg-intro-desktop.9cea2f98.svg"
+  },
+  {
+    "revision": "da0fb76609225c4afc92d7527e42db11",
+    "url": "/img/bg-intro-mobile.da0fb766.svg"
+  },
+  {
+    "revision": "79e698e3f553b89d024f239001979903",
+    "url": "/img/landing-mockup.79e698e3.png"
+  },
+  {
+    "revision": "3884dec1a18d6305e8f1b67c787c9f5e",
+    "url": "/img/splash/ipad_splash.png"
+  },
+  {
+    "revision": "9b77e1c771212b4f20a306c49ac7f0d6",
+    "url": "/img/splash/ipadpro1_splash.png"
+  },
+  {
+    "revision": "277afea471e2dfcea876c27cf9f41cb5",
+    "url": "/img/splash/ipadpro2_splash.png"
+  },
+  {
+    "revision": "4b4290688bc840b79a319fa3e5f67c86",
+    "url": "/img/splash/ipadpro3_splash.png"
+  },
+  {
+    "revision": "5e212c4406692d969b495c1947075ce5",
+    "url": "/img/splash/iphone5_splash.png"
+  },
+  {
+    "revision": "1e1522b73cb42c3f8cfc4561a8dea034",
+    "url": "/img/splash/iphone6_splash.png"
+  },
+  {
+    "revision": "8caddac11c01496fee169fd16ce42c79",
+    "url": "/img/splash/iphoneplus_splash.png"
+  },
+  {
+    "revision": "e0fc6150653b9e1ec910783631dcdcf3",
+    "url": "/img/splash/iphonex_splash.png"
+  },
+  {
+    "revision": "6b1cd573a874ea6787149f4afc646138",
+    "url": "/img/splash/iphonexr_splash.png"
+  },
+  {
+    "revision": "7080cd01ae3ea4f0080625f35dde5b8b",
+    "url": "/img/splash/iphonexsmax_splash.png"
+  },
+  {
+    "revision": "e6cf2652b55f27459f4167cc6a9b85ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d243f3d8574662319935",
+    "url": "/js/app.b8a8474d.js"
+  },
+  {
+    "revision": "d94740290b42b991db44",
+    "url": "/js/chunk-vendors.81e6aeb4.js"
+  },
+  {
+    "revision": "889c5877b068cde610cad8d5755c8bd2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
